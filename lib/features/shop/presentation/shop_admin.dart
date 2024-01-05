42b8035eda0f857860e245f7de4c2d09047f4ff7
OJT User 3 <ojt3@DBIC.local>
2024-01-05 13:25:53 +0800

    shop_new, shopdropdown, brand_new added

---
+++ lib/features/shop/presentation/shop_admin.dart
@@ -0,0 +1,326 @@
+
+import 'package:flutter/material.dart';
+import 'package:shop_ui/features/branch/presentation/branchadd.dart';
+import 'package:shop_ui/features/shop/presentation/shopdropdown.dart';
+import 'package:shop_ui/features/shop/presentation/shopinfo.dart';
+// import 'package:shop_ui/features/branch/presentation/branchdash.dart';
+import 'package:sidebarx/sidebarx.dart';
+
+class ShopAdminPage extends StatefulWidget {
+  const ShopAdminPage({super.key, 
+  // required this.shopModel
+  });
+  // final ShopModel shopModel;
+
+  @override
+  State<ShopAdminPage> createState() => _SidebarXExampleAppState();
+}
+
+class _SidebarXExampleAppState extends State<ShopAdminPage> {
+  final _controller = SidebarXController(selectedIndex: 0, extended: false);
+
+  final _key = GlobalKey<ScaffoldState>();
+  // late ShopModel _shopModel;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _shopModel = widget.shopModel;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // final isSmallScreen = MediaQuery.of(context).size.width < 600;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      key: _key,
+      appBar:
+          // isSmallScreen
+          //     ?
+          AppBar(
+          
+                  backgroundColor: canvasColor,
+                  title: const Text(''),
+                  // leading: IconButton(
+                  //   onPressed: () {
+                  //     // if (!Platform.isAndroid && !Platform.isIOS) {
+                  //     //   _controller.setExtended(true);
+                  //     // }
+                  //     _key.currentState?.openDrawer();
+                  //   },
+                  //   icon: const Icon(Icons.menu, color: white,),
+                  // ),
+                  actions:  [
+          const Tooltip(
+            message: 'Admin',
+            child: Icon(Icons.person, color: white,)),
+          const SizedBox(width: 10,),
+          
+          IconButton(onPressed: (
+          ){
+          
+            // Navigator.push(context, 
+            // MaterialPageRoute(builder: (context) => const LoginPage()));
+          
+          }, icon: const Icon(Icons.power_settings_new_rounded, color: white,)),
+          const SizedBox(width: 20,),
+                  ],
+                  
+                ),
+      // drawer: Sidebar(controller: _controller),
+      // : null,
+      body: Row(
+        children: [
+          SidebarX(
+            controller: _controller,
+            theme: SidebarXTheme(
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: canvasColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              hoverColor: scaffoldBackgroundColor,
+              textStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+              selectedTextStyle: const TextStyle(color: Colors.white),
+              itemTextPadding: const EdgeInsets.only(left: 30),
+              selectedItemTextPadding: const EdgeInsets.only(left: 30),
+              itemDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: canvasColor),
+              ),
+              selectedItemDecoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: actionColor.withOpacity(0.37),
+                ),
+                gradient: const LinearGradient(
+                  colors: [accentCanvasColor, canvasColor],
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.28),
+                    blurRadius: 30,
+                  )
+                ],
+              ),
+              iconTheme: IconThemeData(
+                color: Colors.white.withOpacity(0.7),
+                size: 20,
+              ),
+              selectedIconTheme: const IconThemeData(
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+            extendedTheme: const SidebarXTheme(
+              width: 230,
+              decoration: BoxDecoration(
+                color: canvasColor,
+              ),
+            ),
+            // footerDivider: divider,
+            headerBuilder: (context, extended) {
+              return  const SizedBox(
+                height: 100,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  // child: Image.asset('images/house2.jpg')
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Icon(
+                      Icons.shopify_rounded,
+                      color: white,
+                      size: 50,
+                    ),
+                  )
+                  // Text('Image', selectionColor: Colors.white,)
+                  // Image.asset('assets/images/avatar.png')
+                  ,
+                ),
+              );
+            },
+            items: [
+              SidebarXItem(
+                icon: Icons.home,
+                label: 'Home',
+                onTap: () {
+                  // debugPrint('Home');
+                },
+              ),
+              
+              const SidebarXItem(
+                icon: Icons.add_circle,
+                label: 'Register Branch',
+              ),
+              const SidebarXItem(
+                icon: Icons.info,
+                label: 'Shop Info',
+              ),
+              const SidebarXItem(
+                icon: Icons.published_with_changes_rounded,
+                label: 'Transaction',
+              ),
+              const SidebarXItem(
+                icon: Icons.report_gmailerrorred_rounded,
+                label: 'Report',
+              ),
+              const SidebarXItem(
+                icon: Icons.library_books,
+                label: 'List',
+              ),
+              const SidebarXItem(
+                icon: Icons.build_circle_outlined,
+                label: 'Shop Maintenance',
+              ),
+              const SidebarXItem(
+                icon: Icons.attribution_sharp,
+                label: 'Employee Maintenance',
+              ),
+              // const SidebarXItem(
+              //   icon: Icons.person,
+              //   label: 'Employees',
+              // ),
+            ],
+            // footerItems: [
+            //   SidebarXItem(
+            //     icon: Icons.power_settings_new_rounded,
+            //     label: 'Logout',
+            //     onTap: () {
+            //       // debugPrint('Logout');
+            //       // Navigator.push(context,
+            //       //     MaterialPageRoute(builder: (context) => const LoginPage()));
+            //     },
+            //   )
+            // ],
+          ),
+          Expanded(
+            child: Center(
+              child: _ScreensExample(
+                controller: _controller,
+                // shopModel: _shopModel,
+              ),
+            ),
+          ),
+        ],
+      )
+      // ExampleSidebarX(controller: _controller)
+      ,
+      // body: Row(
+      //   children: [
+      //     Expanded(
+      //       child: Center(
+      //         child: _ScreensExample(
+      //           controller: _controller,
+      //           // shopModel: _shopModel,
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      bottomNavigationBar: const BottomAppBar(
+              color: canvasColor,
+              surfaceTintColor: canvasColor,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Copyright © 2023', style: TextStyle(
+                    color: white
+                  ),),
+                ],
+              ),
+             ),
+      
+    );
+  }
+}
+
+class _ScreensExample extends StatefulWidget {
+  const _ScreensExample({
+    required this.controller,
+    // required this.shopModel,
+  });
+
+  final SidebarXController controller;
+  // final ShopModel shopModel;
+
+  @override
+  State<_ScreensExample> createState() => _ScreensExampleState();
+}
+
+class _ScreensExampleState extends State<_ScreensExample> {
+  // late ShopModel _shopModel;
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   // _shopModel = widget.shopModel;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return AnimatedBuilder(
+      animation: widget.controller,
+      builder: (context, child) {
+        final pageTitle = _getTitleByIndex(widget.controller.selectedIndex);
+        switch (widget.controller.selectedIndex) {
+          case 0:
+            return const ShopDrop(
+              // shopModel: _shopModel,
+            );
+          case 1:
+            return const AddBranchPage(
+              shopId: '',
+              // shopId: _shopModel.shopId,
+            );
+          case 2:
+            return const InfoShopPage();
+          // case 3:
+          //   return const BranchDash();
+          default:
+            return Text(
+              pageTitle,
+              style: theme.textTheme.headlineSmall,
+            );
+        }
+      },
+    );
+  }
+}
+
+String _getTitleByIndex(int index) {
+  switch (index) {
+    case 0:
+      return 'Home';
+    case 1:
+      return 'Search';
+    case 2:
+      return 'Add';
+    case 3:
+      return 'Transaction';
+    case 4:
+      return 'Report';
+    case 5:
+      return 'List';
+    case 6:
+      return 'Shop Maintenance';
+    case 7:
+      return 'Employee Maintenance';
+    // case 7:
+    //   return 'Logout';
+    default:
+      return 'Not found page';
+  }
+}
+
+const textColor = Colors.brown;
+const primaryColor = Colors.black38;
+const canvasColor = Colors.brown;
+const scaffoldBackgroundColor = Colors.white38;
+const accentCanvasColor = Color.fromRGBO(40, 120, 19, 1);
+const white = Colors.white;
+final actionColor = const Color(0xFF5F5FA7).withOpacity(0.6);
+final divider = Divider(color: white.withOpacity(0.3), height: 1);
+
+
+
+//
---
+++ lib/features/shop/presentation/shopdropdown.dart
@@ -0,0 +1,178 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_ui/features/branch/presentation/branch.dart';
+import 'package:shop_ui/features/branch/presentation/branchdash.dart';
+
+class ShopDrop extends StatefulWidget {
+  const ShopDrop({super.key});
+
+  @override
+  State<ShopDrop> createState() => _ShopDropState();
+}
+
+class _ShopDropState extends State<ShopDrop> {
+  final List<String> items = [
+    'Branch 1',
+    'Branch 2',
+    'Branch 3',
+    'Branch 4',
+    'Branch 5',
+    // 'Branch 6',
+    // 'Branch 7',
+    // 'Branch 8',
+  ];
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                '-SHOP NAME- DASHBOARD',
+                style: GoogleFonts.ptSerif(
+                  textStyle: const TextStyle(
+                    color: Color.fromRGBO(40, 120, 19, 1),
+                    fontSize: 50,
+                    // letterSpacing: .5
+                  ),
+                ),
+              ),
+            ),
+          ),
+          //
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: screenSize.height / 6),
+            child: Center(
+              child: DropdownButtonHideUnderline(
+                child: SizedBox(
+                  width: 500,
+                  child: DropdownButton2<String>(
+                    isExpanded: true,
+                    hint: const Row(
+                      children: [
+                        // Icon(
+                        //   Icons.list,
+                        //   size: 16,
+                        //   color: white,
+                        // ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              'Select Branch',
+                              style: TextStyle(
+                                fontSize: fontsize,
+                                fontWeight: FontWeight.bold,
+                                color: white,
+                              ),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    items: items
+                        .map((String item) => DropdownMenuItem<String>(
+                              alignment: AlignmentDirectional.center,
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                  fontSize: fontsize,
+                                  fontWeight: FontWeight.bold,
+                                  color: white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ))
+                        .toList(),
+                    value: selectedValue,
+                    onChanged: (String? value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    },
+                    buttonStyleData: ButtonStyleData(
+                      height: 50,
+                      width: 160,
+                      padding: const EdgeInsets.only(left: 14, right: 14),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        border: Border.all(
+                          color: accentCanvasColor,
+                        ),
+                        color: canvasColor,
+                      ),
+                      elevation: 2,
+                    ),
+                    iconStyleData: const IconStyleData(
+                      icon: Icon(
+                        Icons.keyboard_arrow_down_rounded,
+                        size: 30,
+                      ),
+                      iconSize: 14,
+                      iconEnabledColor: white,
+                      iconDisabledColor: Colors.grey,
+                    ),
+                    dropdownStyleData: DropdownStyleData(
+                      maxHeight: screenSize.height / 3,
+                      width: 500,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: Colors.brown.shade300,
+                      ),
+                      // offset: const Offset(-20, 0),
+                      scrollbarTheme: ScrollbarThemeData(
+                        radius: const Radius.circular(40),
+                        thickness: MaterialStateProperty.all<double>(6),
+                        thumbVisibility: MaterialStateProperty.all<bool>(true),
+                      ),
+                    ),
+                    menuItemStyleData: const MenuItemStyleData(
+                      height: 40,
+                      padding: EdgeInsets.only(left: 14, right: 14),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 100,
+            child: FloatingActionButton(
+              backgroundColor: canvasColor,
+              foregroundColor: white,
+              splashColor: accentCanvasColor,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const BranchPage()));
+              },
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      'Go',
+                      style: TextStyle(fontSize: fontsize),
+                    ),
+                  ),
+                  Icon(Icons.search),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
