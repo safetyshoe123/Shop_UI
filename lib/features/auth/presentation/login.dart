34b0c017e2bc20574a5aa6d2f46ab4feedc33eb3
OJT User 2 <ojt2@DBIC.local>
2024-01-10 13:53:03 +0800

    printed errors removed

--- lib/features/auth/presentation/login.dart
+++ lib/features/auth/presentation/login.dart
@@ -257,12 +257,10 @@ class _LoginPageState extends State<LoginPage> {
   void _loginListener(BuildContext context, AuthState state) {
     if (state.stateStatus == StateStatus.error) {
       SnackBarUtils.errorSnackBar(state.errorMessage, context);
-      print(state.errorMessage);
     }
 
     if (state.authModel != null) {
       if (state.authModel!.restriction.isEmpty) {
-        print('EMPTY');
         SnackBarUtils.successSnackBar('Login Success', context);
         Navigator.push(
           context,
